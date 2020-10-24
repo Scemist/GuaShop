81995d2949fe4da9427a9dfee1164f79d2fc5e83
Lucas Gonçalves S <scemist.lucas@gmail.com>
2020-10-24 20:28:57 -0300

    Removida tag style da página loginda Loja
    Nathan deixou style na página, eu joguei fora.

--- loja/login.php
+++ loja/login.php
@@ -4,22 +4,25 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Tag de viewport -->
-		<link rel="icon" type="imagem/png" href="../favicon.ico"> <!-- Flavicon -->
 
+		<link rel="icon" type="imagem/png" href="../favicon.ico"> <!-- Flavicon -->
 		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css"> <!-- CSS Bootstrap -->
+		<link rel="stylesheet" href="css/estilo.css">
 
 		<title>GuaShop - Loja</title>
 	</head>
+
 	<body class="bg-light">
 		<main class="container">
 
 			<div class="row my-5 py-4"></div>
 
 			<form action="externo/logar.php" method="POST" enctype="multipart/form-data">
-				<div class="row py-4 px-5 mt-5 bg-white mx-auto rounded shadow" style="width: 60%;">
+				<div class="row py-4 px-5 mt-5 bg-white mx-auto rounded shadow cartao-login">
+
 					<div class="col-12 text-center">
 						<h2 class="my-2 text-black display-4">GuaShop</h2>
-						<p class="my-2">Lojas Login</p>
+						<p class="my-2">Login como loja</p>
 					</div>
 
 					<div class="col-12">
@@ -33,7 +36,7 @@
 					</div>
 
 					<div class="col-12">
-						<input role="button" type="submit" name="enviar" value="Acessar" class="btn btn-outline-primary float-right m-3" href="externo/logar.php">
+						<input role="button" type="submit" name="enviar" value="Acessar" class="btn btn-outline-info float-right m-3" href="externo/logar.php">
 					</div>
 				</div>
 			</form>
