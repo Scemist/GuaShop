e0b2776a86a1489f1a23c0351bf4ef37c2e1c7fb
Lucas Gonçalves S <scemist.lucas@gmail.com>
2020-10-27 20:55:44 -0300

    Revert "Atualização bootstrap para 4.5.3 - utilitario"
    This reverts commit 3105f43a35fa9e523b0a44d68172734f72a61cf9.

--- utilitario/sobre.php
+++ utilitario/sobre.php
@@ -13,7 +13,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Tag de viewport -->
 
 		<link rel="icon" type="imagem/png" href="../favicon.ico"> <!-- Flavicon -->
-		<link rel="stylesheet" href="../bootstrap/bootstrap-4.5.3.min.css"> <!-- CSS Bootstrap -->
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css"> <!-- CSS Bootstrap -->
 		<link rel="stylesheet" type="text/css" href="css/navbar.css"> <!-- CSS NavBar -->
 		<link rel="stylesheet" type="text/css" href="css/fa/css/all.css"> <!-- CSS Ícones -->
 		<link rel="stylesheet" type="text/css" href="css/geral.css"> <!-- CSS Personalizado -->
@@ -78,10 +78,11 @@
         	</div>
 		</main>
 
-  		<?php  require_once('externo/footer.php')  ?>
+    <?php  require_once('externo/footer.php')  ?>
 
-		<script src="../bootstrap/jquery-3.5.1.slim.min.js"></script> <!-- jQuery -->
-		<script src="../bootstrap/bootstrap.bundle-4.5.3.min.js"></script> <!-- Bundle -->
+		<script src="../bootstrap/jquery/jquery-3.3.1.min.js"></script> <!-- jQuery -->
+		<script src="../bootstrap/popper/popper.min.js"></script> <!-- Popper.js -->
+		<script src="../bootstrap/js/bootstrap.min.js"></script> <!-- Bootstrap JS -->
     	<script type="text/javascript" src="jquery/navbar.js"></script> <!-- jQuery NavBar -->
 	</body>
 </html>
