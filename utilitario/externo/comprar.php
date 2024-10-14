0b90d8e284f26e40da26fad2211816b86342e70d
Lucas Scemist <scemist.lucas@gmail.com>
2024-10-13 21:17:54 -0300

    fix: echo before set location header in comprar.php

--- utilitario/externo/comprar.php
+++ utilitario/externo/comprar.php
@@ -1,12 +1,11 @@
 <?php
 
-// Conexão e sessão
 require_once '../../funcoes/php/conexao.php';
 $conexao = estabelecerConexao('utilitario', false);
 
 date_default_timezone_set('America/Sao_Paulo');
 
-echo $total = $_POST['total'];
+$total = $_POST['total'];
 $horario = date('H:i:s');
 $data = date('Y-m-d');
 $estado = 'entregue'; // entregue, finalizado.
@@ -26,21 +25,22 @@ $sql->bindParam(':estado', $estado);
 $sql->bindParam(':endereco', $endereco);
 $sql->bindParam(':usuario', $usuario['id_usua']);
 $sql->bindParam(':destinatario', $_POST['destinatario']);
-if (isset($_POST['cartao'])):
+
+if (isset($_POST['cartao'])) :
 	$cartao = 'cartao';
 	$sql->bindParam(':pagamento', $cartao);
 	$sql->bindParam(':cartao', $_POST['cartao']);
-else:
+else :
 	$nada = null;
 	$sql->bindParam(':pagamento', $null);
 	$sql->bindParam(':cartao', $null);
 endif;
+
 $sql->execute();
 $pedido = $conexao->lastInsertId();
 
 // Cria as intancias de cada pedido
-for ($controle = 0; $controle <= $total; $controle++):
-
+for ($controle = 0; $controle <= $total; $controle++) :
 	${'produto_' . $controle} = $_POST["produto_$controle"];
 	${'produto_quantidade_' . $controle} = $_POST["produto_quantidade_$controle"];
 
